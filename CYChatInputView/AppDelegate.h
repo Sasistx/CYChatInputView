4f470a688c0a24a6d5abea5a30264b3c2466c61d
gaotianxiang <gaotianxiang@chunyu.me>
2016-08-30 15:39:03 +0800

    Initial Commit

---
+++ CYChatInputView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYChatInputView
+//
+//  Created by 高天翔 on 16/8/30.
+//  Copyright © 2016年 CYGTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYChatInputView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYChatInputView
+//
+//  Created by 高天翔 on 16/8/30.
+//  Copyright © 2016年 CYGTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
